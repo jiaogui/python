b6dd84209e558d6c2321459cbcf80c05d8c7d3f9
wen <1919739460@qq.com>
2019-06-17 07:16:43 +0800

    修改登录验证

--- dj/h5/middleware/loginMiddleware.py
+++ dj/h5/middleware/loginMiddleware.py
@@ -5,10 +5,14 @@ class LoginMiddleware(MiddlewareMixin):
 		print(request.path)
 	def process_response(self,request,response):
 		url = request.path
+		# h5下放行的路由
 		bypass = {
 			'/h5/login':'/h5/login',
 			'/h5/loginPy':'/h5/loginPy',
 		}
+		# 后台路由放行
+		if '/admin/' in url:
+			return response
 		if url in bypass:
 			return response
 		else:
